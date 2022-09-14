cd517f9d3beca7ea48fa6b3076a655a95f42cb7b
rafi <rafi1421@gmail.com>
2022-09-14 17:24:29 -0500

    Increase back side probe margin to give more clearance for clamps
    also added 2 probe touches for more accuracy

--- Configuration.h
+++ Configuration.h
@@ -1262,7 +1262,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-//#define MULTIPLE_PROBING 2
+#define MULTIPLE_PROBING 2
 //#define EXTRA_PROBING    1
 
 /**
@@ -1635,7 +1635,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
